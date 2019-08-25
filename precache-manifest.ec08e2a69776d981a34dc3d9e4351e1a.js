5bd557e4ba61e4db11e172f777a313b9e5353e1e
InkyChew <B10623019@yuntech.edu.tw>
2019-08-26 05:45:57 +0800

    Updates

---
+++ precache-manifest.ec08e2a69776d981a34dc3d9e4351e1a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d09663fb43184d47b4c51a6a15202d06",
+    "url": "/img/leaf.d09663fb.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "70a0c1f934ebf9e1d36689c3e75e18ab",
+    "url": "/img/b2.70a0c1f9.jpg"
+  },
+  {
+    "revision": "ecc721dbae0ea55a9950",
+    "url": "/css/app.729c8b02.css"
+  },
+  {
+    "revision": "7009cf92910b0dd61b49",
+    "url": "/js/chunk-vendors.5de009e4.js"
+  },
+  {
+    "revision": "a3fd7961e57fd9837ecc959c287d6aa2",
+    "url": "/img/about.a3fd7961.jpg"
+  },
+  {
+    "revision": "a0416cd85d1a5b033710",
+    "url": "/js/about.5a566621.js"
+  },
+  {
+    "revision": "ecc721dbae0ea55a9950",
+    "url": "/js/app.10cff319.js"
+  },
+  {
+    "revision": "304db1bb471129323cbf292eaba02741",
+    "url": "/img/f1.304db1bb.jpg"
+  },
+  {
+    "revision": "f318d4071a511aab7553b1c230787088",
+    "url": "/img/b3.f318d407.jpg"
+  },
+  {
+    "revision": "127d6fb35cdd77cb123b6595326e1d7b",
+    "url": "/img/b1.127d6fb3.jpg"
+  },
+  {
+    "revision": "d4f8463bca44d0e5c170dde1e15a401f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0416cd85d1a5b033710",
+    "url": "/css/about.00e68aff.css"
+  }
+];
